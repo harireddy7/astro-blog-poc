6b39343eee1713e1f0af7a9bf189a827e093e281
harireddy7 <harimsd8@gmail.com>
2024-09-01 20:47:28 +0530

    add dynamic routes for tags pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true
-  }
+	server: {
+		port: 3000,
+		open: true,
+	},
 });
