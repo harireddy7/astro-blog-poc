97647e02bf569889b320999dd56430a8aa66f73e
harireddy7 <harimsd8@gmail.com>
2024-09-01 22:32:19 +0530

    add preact framework to hydrate components on client side

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	server: {
-		port: 3000,
-		open: true,
-	},
-	site: 'https://astro-blog-poc.netlify.app'
+  server: {
+    port: 3000,
+    open: true
+  },
+  site: 'https://astro-blog-poc.netlify.app',
+  integrations: [preact()]
 });
